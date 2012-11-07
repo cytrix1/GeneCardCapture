417e0d54192a323f2f44fb74f404ec40234ef4e3
Young Soo Song <extramorein@gmail.com>
2012-11-07 20:41:03 +0900

    Files changed

--- genelist2google.js
+++ genelist2google.js
@@ -8,27 +8,20 @@ document.body.appendChild(b);
 //GeneList = ["RB1", "MUC16", "LRP1B", "PKHD1", "TP53", "OR9G1", "MUC17", "APOB", "CHD7", "CTAGE5", "FSIP2", "CSMD3", "DNHD1", "HERC2", "PRAMEF2", "ANKRD30B", "DNAH5", "DNAH7", "DTNBP1", "LAMA1", "PCDH15", "ZNF326", "NOTCH2NL", "OR6N2", "ARSD", "COL12A1", "NBPF10", "PRAMEF1", "PTEN", "SELP", "SGCZ", "USP6", "ACIN1", "ADAM12", "ADAMTS12", "AHNAK", "AK5", "AMMECR1L", "ANKRD20A1", "ANKS1B", "AP2B1", "ATP8B1", "BAGE2", "BOD1L", "C5orf20", "C6", "C7orf72", "C9orf51", "C9orf79", "CCDC88A", "CCNYL2", "CD1C", "CDH12", "CDH9", "CDKN3", "CFHR2", "CHD8", "CHFR", "CNOT1", "CNTN5", "COL5A2", "CREBBP", "CXorf22", "CYTIP", "DCAF4L2", "DDC", "DGKB", "DLGAP2", "DMD", "DSC2", "EARS2", "EPHA5", "EPHA7", "FAT4", "FREM2", "FRG1", "GABRG2", "GABRQ", "GALNT13", "GBP6", "GFI1B", "GLI1", "GPRC6A", "HCN1", "IDO1", "IFRD1", "IRGM", "KCNH7", "KCNQ3", "KIAA1586", "KIF6", "LAMA2", "LAMC1", "LY75-CD302", "MAGEC1", "MAN2A1", "MAP1A", "MEOX2", "MGAT4C"];
 //GeneList = ["RB1", "MUC16", "LRP1B", "PKHD1", "TP53", "OR9G1", "MUC17", "APOB", "CHD7", "CTAGE5", "FSIP2", "CSMD3", "DNHD1", "HERC2", "PRAMEF2", "ANKRD30B", "DNAH5", "DNAH7", "DTNBP1", "LAMA1", "PCDH15", "ZNF326", "NOTCH2NL", "OR6N2", "ARSD", "COL12A1", "NBPF10", "PRAMEF1", "PTEN", "SELP", "SGCZ", "USP6", "ACIN1", "ADAM12"];
 //GeneList = ["RB1", "MUC16", "LRP1B", "PKHD1", "TP53", "OR9G1", "MUC17", "APOB", "CHD7", "CTAGE5", "FSIP2", "CSMD3", "DNHD1", "HERC2"]
-GeneList = ["RB1", "PKHD1", "TP53"];
+GeneList = ["RB1", "MUC16", "LRP1B", "PKHD1", "TP53"];
 
-//for (var i = 0; i < GeneList.length; ++i) {
-//  var di = document.createElement('div');
-//  di.id = 'di' + i;
-//  di.innerHTML = "<h2>" + GeneList[i] + "</h2>";
-//  document.body.appendChild(di);
-//}
+for (var i = 0; i < GeneList.length; ++i) {
+  var di = document.createElement('div');
+  di.id = 'di' + i;
+  di.innerHTML = "<h2>" + GeneList[i] + "</h2>";
+  document.body.appendChild(di);
+}
 
 
 $(document).ready(function() {
   $("#b").click(function(){
     for (var i=0; i < GeneList.length; ++i) {
-      //$("#di" + i).load("/cgi-bin/carddisp.pl?gene=" + GeneList[i]);
-	  //window.open("http://www.genecards.org/cgi-bin/carddisp.pl?gene=" + GeneList[i]);
-	  myWindow = window.open("http://www.genecards.org");
-	  mydoc = myWindow.document;
-	  var di = mydoc.createElement('div');
-	  di.id = 'di' + i;
-	  mydoc.body.appendChild(di);
-	  $("#di" + i).load("/cgi-bin/carddisp.pl?gene=" + GeneList[i]);
+      $("#di" + i).load("/#hl=ko&sclient=psy-ab&q=" + GeneList[i] + "+cancer");
     }
 	});
 });
